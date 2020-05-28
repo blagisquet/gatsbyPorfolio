28fec504a33d0b083f0694fec62e7d04f5b415be
blagisquet <baptiste.lagisquet@imie.fr>
2020-05-28 11:45:12 +0200

    added support of dark theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ThemeProvider } from 'styled-components';
 
 import Layout from '../components/layout';
+import GlobalStyle from '../theme/globalStyle';
+import ThemeButton from '../theme/themeButton';
+import { lightTheme, darkTheme } from '../theme/themes';
 
-const IndexPage = () => <Layout />;
+const IndexPage = () => {
+  const [theme, setTheme] = useState(lightTheme);
 
+  const handleTheme = () => {
+    theme.name === 'light' ? setTheme(darkTheme) : setTheme(lightTheme);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <ThemeButton handleTheme={handleTheme} />
+      <GlobalStyle />
+      <Layout />
+    </ThemeProvider>
+  )
+}
 export default IndexPage;
