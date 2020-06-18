307b06ea378888090327a698d6ac66aa926e3ec9
blagisquet <Baptiste_Lagisquet_student2019@wilder.school>
2020-06-18 12:04:40 +0200

    added style for about me

--- src/components/experiences.js
+++ src/components/experiences.js
@@ -10,7 +10,7 @@ import styled from 'styled-components';
 const Experiences = ({ className }) => {
   return (
     <section className={className}>
-      <div className="container columns">
+      <div className="columns">
         <div className="column exp is-one-third">
           <h3 className="has-text-weight-bold is-pulled-right">Expériences</h3>
         </div>
@@ -18,10 +18,16 @@ const Experiences = ({ className }) => {
           <div>
             <h4 className="has-text-weight-bold">Freelance</h4>
             <p><span>Développeur Web</span> • Mars 2020 - Aujourd'hui</p>
+            <p>Réalisation d'un site web en GatsbyJs pour le studio Septième Sens, conception d'interfaces (Adobe XD), UX Design, méthodologie Agile. </p>
           </div>
           <div>
             <h4 className="has-text-weight-bold">Syndis</h4>
             <p><span>Stagiaire développeur Angular8</span> • Août 2019 - Novembre 2019</p>
+            <p>Conception d'interface (Figma), création des spécifications (OpenAPI), diagramme UML, développement des interfaces sur le site (syndis.fr) ainsi que sur l'application de gestion de copropriété.</p>
+          </div>
+          <div>
+            <h4 className="has-text-weight-bold">Boulangerie Charrier</h4>
+            <p><span>Vendeur</span> • Avril 2018 - Août 2018</p>
           </div>
         </div>
       </div>
@@ -40,7 +46,7 @@ export default styled(Experiences)`
   span {
     font-style: italic;
   }
-  .exp {
+  /* .exp {
     margin-right: 2.6rem;
   }
   @media (min-width:769px) {
@@ -52,5 +58,5 @@ export default styled(Experiences)`
   .experiences{
     margin: 0 1rem;
   }
-}
+} */
 `;
